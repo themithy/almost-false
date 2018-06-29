05a9e7ae0953bf5af7ab0c9cb5cbb03cc46925df
michal <michal@localhost>
2018-06-29 12:21:12 +0200

    Created

---
+++ index.js
@@ -0,0 +1,37 @@
+
+function almostAlwaysTrue(a) {
+  console.log(a == a)
+}
+
+almostAlwaysTrue(NaN) // false - this is easy
+
+// -----
+
+function usuallyFalse(a) {
+  console.log(!a == a)
+}
+
+usuallyFalse('0')                 // true
+usuallyFalse(' ')                 // true
+usuallyFalse([])                  // true
+usuallyFalse([undefined])         // true
+usuallyFalse([null])              // true
+usuallyFalse([false])             // false - outlier
+usuallyFalse([0])                 // true
+usuallyFalse([''])                // true
+usuallyFalse([' '])               // true
+usuallyFalse(['0'])               // true
+usuallyFalse(new Boolean(false))  // true - in fact, this is the most bizarre
+usuallyFalse(new String())        // true
+
+// -----
+
+function inMostCasesTrue(a) {
+  console.log(a >= a)
+}
+
+inMostCasesTrue(NaN)        // false
+inMostCasesTrue(undefined)  // false
+
+// -----
+
